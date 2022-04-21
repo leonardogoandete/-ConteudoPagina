97ddcf8787386fd231deca849c5b07c7475dc385
Leonardo Goandete <leonardo.goandete@hotmail.com>
2022-04-20 22:54:11 -0300

    Primeiro commit

---
+++ src/LoadPage.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+
+public class LoadPage {
+    public void getPage(URL url) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            // Imprime página no console
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
+
+    public static void main(String[] args) {
+        // le a URL
+        System.out.println("Digite a sua URL:\n");
+        Scanner ler = new Scanner(System.in);
+        String url_lida = ler.nextLine();
+        try {
+            URL url = new URL(url_lida);
+            new LoadPage().getPage(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
